9acad9c1acb45d18a563a1d469fa11929195d290
Jason Wu <wujiechen@gmail.com>
2016-04-27 14:22:51 +0800

    Add seminar.new page

--- app/controllers/seminars_controller.rb
+++ app/controllers/seminars_controller.rb
@@ -2,5 +2,14 @@ class SeminarsController < ApplicationController
   def index
   end
   def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.new(params[:post].permit(:title,:content))
+    if @post.save
+      redirect_to action: "new"
+    else
+      render 'new'
+    end
   end
 end
