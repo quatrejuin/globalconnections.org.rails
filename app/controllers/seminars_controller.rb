3abfb444ac937e8e4cc38974cb86b978a6cf9c4d
Jason Wu <wujiechen@gmail.com>
2016-05-17 15:04:24 +0800

    Add  Show/Edit/Delete

--- app/controllers/seminars_controller.rb
+++ app/controllers/seminars_controller.rb
@@ -1,15 +1,44 @@
 class SeminarsController < ApplicationController
+  include SeminarsHelper
+
   def index
+    @seminars = Post.all
   end
+
+  def show
+    @seminar = Post.find(params[:id])
+  end
+
   def new
-    @post = Post.new
+    @seminar = Post.new
   end
+
   def create
-    @post = Post.new(params[:post].permit(:title,:content))
-    if @post.save
+    @seminar = Post.new(params[:post].permit(:title,:content))
+    if @seminar.save
       redirect_to action: "new"
     else
       render 'new'
     end
+  end
+
+  def edit
+    @seminar = Post.find(params[:id])
+  end
+
+  def update
+    @seminar = Post.find(params[:id])
+    if @seminar.update_attributes(params[:post].permit(:title,:content))
+      flash[:success] = "Seminar updated"
+      redirect_to @seminar
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    Post.find(params[:id]).destroy
+    flash[:success] = "Seminar deleted"
+    redirect_to seminars_url
   end
 end
--- app/helpers/seminars_helper.rb
+++ app/helpers/seminars_helper.rb
@@ -1,2 +1,18 @@
 module SeminarsHelper
+  def post_path(seminar)
+    seminar_path(seminar)
+  end
+
+  # poats_path will be used in new.html.erb by form_for
+  def posts_path
+    seminars_path
+  end
+
+  def post_url(seminar)
+    seminar_url(seminar)
+  end
+
+  def posts_url
+    seminars_url
+  end
 end
