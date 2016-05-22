e976a95918d7f3ca42e673289f8687c8660df1a7
Jason Wu <wujiechen@gmail.com>
2016-05-22 12:37:10 +0800

    AddOrderToPost

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160420153143) do
+ActiveRecord::Schema.define(version: 20160520071540) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160420153143) do
     t.boolean  "isarchived"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "order"
   end
 
 end
