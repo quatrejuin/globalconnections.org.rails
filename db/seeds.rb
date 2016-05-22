56e6df69e3a65fc1cde8f14fd9aed0c0ecfe014e
Jason Wu <wujiechen@gmail.com>
2016-05-22 12:37:35 +0800

    Add some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+[
+  ["South Africa & Botswana, 1997", "", 1],
+  ["Australia, Victoria and New South Wales, 1998", "", 2],
+  ["Crowthorne, United Kingdom, 1999", "", 3],
+  ["Massachusetts, United States of America, 2000", "", 4],
+  ["Kenya, Africa, 2001", "", 5],
+  ["China, 2002", "", 6],
+  ["France, 2003", "", 7],
+  ["Bulgaria and Romania, 2004", "", 8],
+  ["Guatemala, 2005", "", 9],
+  ["Connecticut, United States of America, 2007", "", 10],
+  ["Chiang Mai, Thailand, 2008", "", 11],
+  ["Amman, Jordan, 2010", "", 12],
+  ["South Africa & Botswana, 2011", "", 13],
+  ["New Zealand, 2012", "", 14],
+  ["Colombia, 2013", "", 15]
+].each do |title, content, order|
+  Post.create( title: title, content: content, order: order )
+end
