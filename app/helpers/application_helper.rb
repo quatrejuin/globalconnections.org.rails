41df07e9c79155e320f603272da412117234236d
Jason Wu <wujiechen@gmail.com>
2016-10-18 03:18:41 +0800

    1. Correct the page links 2. Set the body class and id

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def gc_body
+    gc_body_id_class = {seminars_show: ["news_and_initiatives","tier-1 layout-n0-p1-s0"],
+    static_pages_home: ["home","tier-2 layout-n0-p1-s1"]}
+    gc_body_id_class[(controller_name+"_"+action_name).to_sym] or ["home","tier-2 layout-n0-p1-s1"]
+  end
 end
